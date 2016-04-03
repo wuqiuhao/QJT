a10467e6660c5a0fcdf34c455ccb090d09bbc9f5
wuqiuhao <750249534@qq.com>
2016-04-03 16:01:27 +0800

    json解析更新

--- QJT/Module/RegLogin/LoginViewController.swift
+++ QJT/Module/RegLogin/LoginViewController.swift
@@ -7,32 +7,31 @@
 //
 
 import UIKit
-
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        NetWorkManager.httpRequest(Methods.login_studentLogin, params: ["userName":"2012812044","passWord":"2012812044"], modelType: Student.self,completed: { (responseData) in
-                print(responseData)
-            }) { (errorMsg) in
-                debugPrint(errorMsg!)
+        NetWorkManager.httpRequest(Methods.test, params: ["userName":"","passWord":""], modelType: Student(), completed: { (responseData) in
+
+        }) { (errorMsg) in
+            debugPrint(errorMsg!)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

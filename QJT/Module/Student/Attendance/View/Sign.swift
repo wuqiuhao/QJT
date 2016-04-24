2ac9d8b8d5fd62fde57d55a34e504320cd2a6870
wuqiuhao <750249534@qq.com>
2016-04-24 15:13:00 +0800

    更新

--- QJT/Module/Student/Attendance/View/Sign.swift
+++ QJT/Module/Student/Attendance/View/Sign.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Attendance: UIView {
+class Sign: UIView {
 
     /*
     // Only override drawRect: if you perform custom drawing.
--- QJT/Module/Student/Attendance/ViewController/SignMainViewController.swift
+++ QJT/Module/Student/Attendance/ViewController/SignMainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AttendanceMainViewController: UIViewController {
+class SignMainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

7f28399017b76ea1bc45cb646e35a6bd3762bae9
LZQ <lizq1013@163.com>
2016-04-26 21:08:34 +0800

    update

--- QJT/Module/Student/Personal/ViewController/SPersonalMainViewController.swift
+++ QJT/Module/Student/Personal/ViewController/SPersonalMainViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SPersonalMainViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var headerView: TableViewHeaderView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,16 +33,38 @@ class SPersonalMainViewController: UIViewController {
 extension SPersonalMainViewController {
     
     func configUI() {
-        
-        tableView.tableHeaderView = SPersonalHeaderView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, 150))
+        tableView.tableFooterView = UIView()
+        headerView = TableViewHeaderView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, 250))
+//        headerView.delegate = self
+        headerView.dataSource = self
+        tableView.tableHeaderView = headerView
+        headerView.backgroundImage = UIImage(named: "1")
     }
     
+}
+
+// MARK: - TableViewHeaderViewDelegate
+//extension SPersonalMainViewController: TableViewHeaderViewDelegate {
+//    func tableViewDuringScrollingAnnimation(contentView: TableHeaderContentView, offset: CGPoint) {
+//        headerView.reloadData()
+//    }
+//}
+
+// MARK: - TableViewHeaderViewDataSource
+extension SPersonalMainViewController: TableViewHeaderViewDataSource {
+    func tableHeaderView(tableHeaderView: UIView) -> UIView {
+        return UIView()
+    }
 }
 
 // MARK: - UITableViewDelegate
 extension SPersonalMainViewController: UITableViewDelegate {
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 45
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+           headerView.layoutHeaderView(scrollView.contentOffset)
     }
 }
 
@@ -52,28 +75,21 @@ extension SPersonalMainViewController: UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("SPersonalTbCell", forIndexPath: indexPath) as! SPersonalTbCell
-        return cell
         
+        let cell = tableView.dequeueReusableCellWithIdentifier("SPersonMainCell", forIndexPath: indexPath) as! SPersonMainCell
+        switch indexPath.row {
+        case 0:
+            cell.typeImg.image = UIImage(named: "SPersonal_leave")
+            cell.titleLbl.text = "请假记录"
+        case 1:
+            cell.typeImg.image = UIImage(named: "SPersonal_attendance")
+            cell.titleLbl.text = "签到记录"
+        case 2:
+            cell.typeImg.image = UIImage(named: "SPersonal_account")
+            cell.titleLbl.text = "账号安全"
+        default:
+            print("")
+        }
+        return cell
     }
 }
-
-//class SPersonalTbCell: UITableViewCell {
-//    
-//    @IBOutlet weak var partTableView: UITableView!
-//    
-//    
-////    switch indexPath.row {
-////    case 0:
-////    cell.typeImg.image = UIImage(named: "SPersonal_leave")
-////    cell.titleLbl.text = "请假记录"
-////    case 1:
-////    cell.typeImg.image = UIImage(named: "SPersonal_attendance")
-////    cell.titleLbl.text = "签到记录"
-////    case 2:
-////    cell.typeImg.image = UIImage(named: "SPersonal_account")
-////    cell.titleLbl.text = "账号安全"
-////    default:
-////    print("")
-////    }
-//}

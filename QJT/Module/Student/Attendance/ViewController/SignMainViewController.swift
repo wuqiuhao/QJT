0cdff7d8be75f8914856151306b869d259307364
wuqiuhao <750249534@qq.com>
2016-04-26 21:51:51 +0800

    更新

--- QJT/Module/Student/Attendance/ViewController/SignMainViewController.swift
+++ QJT/Module/Student/Attendance/ViewController/SignMainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class SignMainViewController: UIViewController,CLLocationManagerDelegate {
+class SignMainViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var latLabel: UILabel!
@@ -21,7 +21,7 @@ class SignMainViewController: UIViewController,CLLocationManagerDelegate {
     
     var locationManager: CLLocationManager?
     var center: CLLocationCoordinate2D?
-    var isLoaded = false
+    var isPlease = false
     var point: MKPointAnnotation?
     
     override func viewDidLoad() {
@@ -29,76 +29,13 @@ class SignMainViewController: UIViewController,CLLocationManagerDelegate {
         // 设置UI显示
         configUI()
         // 设置基本图层
+        mapView.delegate = self
         mapView?.mapType = MKMapType.Standard
-        // 初始化定位
-        self.initLocationManager()
-        self.pleaseWait()
+        mapView?.showsUserLocation = true
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    @IBAction func repositionBtn(sender: AnyObject) {
-        isLoaded = false
-        mapView.removeAnnotation(point!)
-        self.pleaseWait()
-    }
-    
-    func initLocationManager() {
-        locationManager = CLLocationManager()
-        locationManager!.delegate = self
-        locationManager!.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager!.requestWhenInUseAuthorization()
-    }
-    
-    //MARK: CoreLocationManagerDelegate
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        center = mapView.userLocation.coordinate
-        if center?.latitude != 0 && isLoaded != true {
-            
-            print("\(center!.latitude):\(center!.longitude)")
-            self.clearAllNotice()
-            
-            isLoaded = true
-            mapView?.setCenterCoordinate(center!, animated: true)
-            //设置地图范围
-            let latDelta = 0.01
-            let longDelta = 0.01
-            let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
-            //使用当前位置
-            let currentRegion:MKCoordinateRegion = MKCoordinateRegion(center: center!,span: span)
-            //设置显示区域
-            mapView!.setRegion(currentRegion, animated: true)
-            
-            mapView?.showsUserLocation = false
-            latLabel.text = "维度：\(center!.latitude)"
-            longLabel.text = "经度：\(center!.longitude)"
-            
-            point = MKPointAnnotation()
-            point!.coordinate = center!
-            point!.title = "我的位置"
-            mapView.addAnnotation(point!)
-        }
-    }
-    
-    //错误处理
-    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        
-        
-    }
-    
-    //判断是否拥有权限，有，则开始监听位置更新，反之则停止监听
-    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        
-        switch status {
-        case .AuthorizedAlways, .AuthorizedWhenInUse:locationManager!.startUpdatingLocation()
-        mapView?.showsUserLocation = true
-        default:
-            locationManager!.stopUpdatingLocation()
-            mapView?.showsUserLocation = false
-        }
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -110,18 +47,84 @@ class SignMainViewController: UIViewController,CLLocationManagerDelegate {
 
 // MARK: - private method
 extension SignMainViewController {
+    @IBAction func repositionBtn(sender: AnyObject) {
+        mapView.removeAnnotation(point!)
+        startUpdatingLocation()
+    }
+    
+    func initLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager!.delegate = self
+        // 移动10米重新定位
+        locationManager!.distanceFilter = kCLLocationAccuracyNearestTenMeters
+        // 定位精度
+        locationManager!.desiredAccuracy = kCLLocationAccuracyBest
+        startUpdatingLocation()
+    }
+    
     func configUI() {
-        signBtn.layer.borderWidth = 1
-        signBtn.layer.borderColor = UIColor.cyanColor().CGColor
-        signBtn.layer.cornerRadius = 8
-        signBtn.backgroundColor = UIColor.clearColor()
-        
-        repositionBtn.layer.borderWidth = 1
-        repositionBtn.layer.borderColor = UIColor.cyanColor().CGColor
-        repositionBtn.layer.cornerRadius = 8
-        repositionBtn.backgroundColor = UIColor.clearColor()
-        
+        navigationItem.title = "学生签到"
+        signBtn.layer.cornerRadius = 5
+        signBtn.backgroundColor = UIColor.qjtTintColor()
+        signBtn.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+
+        repositionBtn.layer.cornerRadius = 5
+        repositionBtn.backgroundColor = UIColor.qjtTintColor()
+        repositionBtn.setTitleColor(UIColor.whiteColor(), forState: .Normal)
         messageBack.layer.cornerRadius = 8
         messageBack.alpha = 0.6
+        view.bringSubviewToFront(messageBack)
+    }
+    
+    /**
+     开始定位
+     */
+    func startUpdatingLocation() {
+        locationManager?.startUpdatingLocation()
+        self.pleaseWait()
+        isPlease = true
+    }
+}
+
+// MARK: - CoreLocationManagerDelegate
+extension SignMainViewController: CLLocationManagerDelegate {
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if isPlease {
+            isPlease = false
+            self.clearAllNotice()
+        }
+        center = mapView?.userLocation.coordinate
+        print("\(center!.latitude):\(center!.longitude)")
+        mapView?.setCenterCoordinate(center!, animated: true)
+        //设置地图范围
+        let latDelta = 0.01
+        let longDelta = 0.01
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        //使用当前位置
+        let currentRegion:MKCoordinateRegion = MKCoordinateRegion(center: center!,span: span)
+        //设置显示区域
+        mapView!.setRegion(currentRegion, animated: true)
+        
+        mapView?.showsUserLocation = false
+        latLabel.text = "纬度：\(center!.latitude)"
+        longLabel.text = "经度：\(center!.longitude)"
+        
+        point = MKPointAnnotation()
+        point!.coordinate = center!
+        point!.title = "我的位置"
+        mapView.addAnnotation(point!)
+        manager.stopUpdatingLocation()
+    }
+    
+    //错误处理
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        
+    }
+}
+
+extension SignMainViewController: MKMapViewDelegate {
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        // 初始化定位
+        self.initLocationManager()
     }
 }

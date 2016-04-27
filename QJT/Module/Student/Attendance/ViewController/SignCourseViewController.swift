e8c9fb079929df7e21ace72ec9963096f03718ad
chaoyiman <2548364460@qq.com>
2016-04-27 22:41:39 +0800

    更新

--- QJT/Module/Student/Attendance/ViewController/SignCourseViewController.swift
+++ QJT/Module/Student/Attendance/ViewController/SignCourseViewController.swift
@@ -23,36 +23,7 @@ class SignCourseViewController: UIViewController {
         super.viewDidLoad()
         configUI()
         setupCourseBackground()
-//        getNetwork()
-        
-        print("\(self.lat!):\(self.long!)")
-        
-        let model = CourseClass()
-        model.address = "B-301"
-        model.courseClassID = 104
-        model.week = Week.Tuesday
-        model.fromSection = 1
-        model.durationSection = 2
-        model.courseName = "C语言设计"
-        courseArrData.append(model)
-    
-        let model1 = CourseClass()
-        model1.address = "B-301"
-        model1.courseClassID = 104
-        model1.week = Week.Friday
-        model1.fromSection = 7
-        model1.durationSection = 3
-        model1.courseName = "C语言设计"
-        courseArrData.append(model1)
-        
-        let model2 = CourseClass()
-        model2.address = "B-301"
-        model2.courseClassID = 104
-        model2.week = Week.Monday
-        model2.fromSection = 7
-        model2.durationSection = 3
-        model2.courseName = "C语言设计"
-        courseArrData.append(model2)
+        getNetwork()
         
         self.configCourseExcel()
         

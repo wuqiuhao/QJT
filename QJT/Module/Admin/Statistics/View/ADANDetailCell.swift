25e98a34be3ada012718b5c6976244c666dc47bc
LZQ <lizq1013@163.com>
2016-05-02 20:57:15 +0800

    update

---
+++ QJT/Module/Admin/Statistics/View/ADANDetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  ADANDetailCell.swift
+//  QJT
+//
+//  Created by LZQ on 16/5/2.
+//  Copyright © 2016年 Hale. All rights reserved.
+//
+
+import UIKit
+
+class ADANDetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
